c292fc76d4309d1acbac0c6101ccf60ee3d85782
Natalia Zolotova <natakart@inbox.ru>
2026-01-17 23:42:41 +0300

    add structure of project

---
+++ src/classes.py
@@ -0,0 +1,15 @@
+
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+
+
+class Category:
+    name: str
+    description: str
+    products: list
+    category_count
+    product_count
